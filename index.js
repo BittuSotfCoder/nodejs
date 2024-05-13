80fbf3cea6f64bdc20b571d30e57c3c137d90394
BITTU KUMAR <169143833+BittuSotfCoder@users.noreply.github.com>
2024-05-13 20:44:17 +0530

    Node js server

---
+++ index.js
@@ -0,0 +1,45 @@
+const express= require("express");
+const app=express();
+const users=require("./MOCK_DATA.json");
+const PORT=800;
+// Route
+app.get('/user',(req,res)=>{
+        const html=`
+        <ul>
+            ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
+        </ul>
+        `
+        res.send(html);
+
+});
+
+app.get('/API/user',(req,res)=>{
+    return res.send(users);
+});
+
+
+app.get('/API/user/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find(user=>user.id===id);
+       return res.json(user);
+});
+
+app.post('/API/user',(req,res)=>{
+return res.status('pENAAW');
+});
+
+app.patch('/API/user/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find(user=>user.id===id);
+       return res.json(user);
+});
+
+app.delete('/API/user/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find(user=>user.id===id);
+       return res.json(user);
+});
+
+
+app.listen(PORT,()=> console.log(`Server Started`));
+
---
+++ ser1/index.js
@@ -0,0 +1,9 @@
+const express =require("express");
+
+const app=express();
+// const users=require("./MOCK_DATA.json");
+const PORT=9000;
+
+
+
+app.listen(PORT,()=>console.log('Starter server'));
